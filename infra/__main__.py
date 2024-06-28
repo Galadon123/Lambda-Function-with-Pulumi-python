6b46b2aa163afe32d6c9f7a9a834e42c1b7a61b4
Fazlul Karim <fazlulkarim362@gmail.com>
2024-06-29 03:00:08 +0600

    fasfa

--- infra/__main__.py
+++ infra/__main__.py
@@ -6,21 +6,21 @@ vpc = aws.ec2.Vpc("my-vpc",
                   cidr_block="10.0.0.0/16",
                   tags={"Name": "my-vpc"})
 
-# Create a private subnet
+# Create a private subnet within the VPC
 private_subnet = aws.ec2.Subnet("private-subnet",
                                 vpc_id=vpc.id,
                                 cidr_block="10.0.1.0/24",
                                 availability_zone="us-east-1a",
                                 tags={"Name": "private-subnet"})
 
-# Create an ECR repository
+# Create an ECR repository for Lambda functions
 ecr_repo = aws.ecr.Repository("my-ecr-repo",
                               image_tag_mutability="MUTABLE",
                               image_scanning_configuration=aws.ecr.RepositoryImageScanningConfigurationArgs(
                                   scan_on_push=True),
                               tags={"Name": "my-ecr-repo"})
 
-# Create an IAM role for Lambda
+# Create an IAM role for Lambda with an assume role policy allowing Lambda service
 lambda_role = aws.iam.Role("lambda-role",
                            assume_role_policy=aws.iam.get_policy_document(statements=[aws.iam.GetPolicyDocumentStatementArgs(
                                actions=["sts:AssumeRole"],
@@ -31,7 +31,7 @@ lambda_role = aws.iam.Role("lambda-role",
                                effect="Allow",
                            )]).json)
 
-# Attach a policy to the Lambda role that allows necessary actions
+# Attach policies to the Lambda role for necessary actions
 lambda_policy = aws.iam.RolePolicy("lambda-policy",
                                    role=lambda_role.id,
                                    policy=aws.iam.get_policy_document(statements=[
@@ -53,8 +53,7 @@ lambda_policy = aws.iam.RolePolicy("lambda-policy",
                                    ]).json,
                                    opts=pulumi.ResourceOptions(depends_on=[lambda_role]))
 
-
-# Create a security group for Lambda
+# Create a security group for the Lambda function in the VPC
 lambda_security_group = aws.ec2.SecurityGroup("lambda-security-group",
                                               vpc_id=vpc.id,
                                               egress=[aws.ec2.SecurityGroupEgressArgs(
@@ -65,7 +64,7 @@ lambda_security_group = aws.ec2.SecurityGroup("lambda-security-group",
                                               )],
                                               tags={"Name": "lambda-security-group"})
 
-# Export the VPC ID, Subnet ID, Security Group ID, ECR Repository URL, and Lambda Role ARN
+# Export the created resources' identifiers for easy access and reference
 pulumi.export("vpc_id", vpc.id)
 pulumi.export("private_subnet_id", private_subnet.id)
 pulumi.export("ecr_repo_url", ecr_repo.repository_url)
