fd811efe2c305a55743c8e3f274a4623b71264b4
Fazlul Karim <fazlulkarim362@gmail.com>
2024-06-29 13:11:09 +0600

    pulumi

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,89 +1,103 @@
 import pulumi
 import pulumi_aws as aws
 
-# Create a VPC
-vpc = aws.ec2.Vpc("my-vpc",
-                  cidr_block="10.0.0.0/16",
-                  tags={"Name": "my-vpc"})
+class VPC:
+    def __init__(self, name: str, cidr_block: str):
+        self.vpc = aws.ec2.Vpc(name,
+                               cidr_block=cidr_block,
+                               tags={"Name": name})
+        pulumi.export("vpc_id", self.vpc.id)
 
-# Create a private subnet
-private_subnet = aws.ec2.Subnet("private-subnet",
-                                vpc_id=vpc.id,
-                                cidr_block="10.0.1.0/24",
-                                availability_zone="us-east-1a",
-                                tags={"Name": "private-subnet"})
 
-# Create an ECR repository
-ecr_repo = aws.ecr.Repository("my-lambda-function",
-                              image_scanning_configuration={"scanOnPush": True},
-                              tags={"Name": "my-lambda-function"})
+class Subnet:
+    def __init__(self, name: str, vpc_id: pulumi.Output[str], cidr_block: str, availability_zone: str):
+        self.subnet = aws.ec2.Subnet(name,
+                                     vpc_id=vpc_id,
+                                     cidr_block=cidr_block,
+                                     availability_zone=availability_zone,
+                                     tags={"Name": name})
+        pulumi.export("private_subnet_id", self.subnet.id)
 
-# Create an IAM role for Lambda
-lambda_role = aws.iam.Role("lambda-role",
-                           assume_role_policy="""{
-                               "Version": "2012-10-17",
-                               "Statement": [
-                                   {
-                                       "Action": "sts:AssumeRole",
-                                       "Principal": {
-                                           "Service": "lambda.amazonaws.com"
-                                       },
-                                       "Effect": "Allow",
-                                       "Sid": ""
-                                   }
-                               ]
-                           }""")
 
-lambda_policy = aws.iam.RolePolicy("lambda-policy",
-                                   role=lambda_role.id,
-                                   policy="""{
-                                       "Version": "2012-10-17",
-                                       "Statement": [
-                                           {
-                                               "Effect": "Allow",
-                                               "Action": [
-                                                   "logs:CreateLogGroup",
-                                                   "logs:CreateLogStream",
-                                                   "logs:PutLogEvents"
-                                               ],
-                                               "Resource": "arn:aws:logs:*:*:*"
-                                           },
-                                           {
-                                               "Effect": "Allow",
-                                               "Action": [
-                                                   "ecr:GetDownloadUrlForLayer",
-                                                   "ecr:BatchGetImage",
-                                                   "ecr:BatchCheckLayerAvailability"
-                                               ],
-                                               "Resource": "*"
-                                           },
-                                           {
-                                               "Effect": "Allow",
-                                               "Action": [
-                                                   "ec2:CreateNetworkInterface",
-                                                   "ec2:DescribeNetworkInterfaces",
-                                                   "ec2:DeleteNetworkInterface"
-                                               ],
-                                               "Resource": "*"
-                                           }
-                                       ]
-                                   }""")
+class ECRRepository:
+    def __init__(self, name: str):
+        self.repository = aws.ecr.Repository(name,
+                                             image_scanning_configuration={"scanOnPush": True},
+                                             tags={"Name": name})
+        pulumi.export("ecr_repo_url", self.repository.repository_url)
+        pulumi.export("ecr_registry", self.repository.registry_id)
 
-# Create a security group for Lambda
-lambda_security_group = aws.ec2.SecurityGroup("lambda-security-group",
-                                              vpc_id=vpc.id,
-                                              egress=[{
-                                                  "protocol": "-1",
-                                                  "from_port": 0,
-                                                  "to_port": 0,
-                                                  "cidr_blocks": ["0.0.0.0/0"],
-                                              }],
-                                              tags={"Name": "lambda-security-group"})
 
-# Export the VPC ID and Subnet ID
-pulumi.export("vpc_id", vpc.id)
-pulumi.export("private_subnet_id", private_subnet.id)
-pulumi.export("ecr_repo_url", ecr_repo.repository_url)
-pulumi.export("ecr_registry", ecr_repo.registry_id)
-pulumi.export("lambda_role_arn", lambda_role.arn)
-pulumi.export("lambda_security_group_id", lambda_security_group.id)
+class LambdaRole:
+    def __init__(self, name: str):
+        self.role = aws.iam.Role(name,
+                                 assume_role_policy="""{
+                                     "Version": "2012-10-17",
+                                     "Statement": [
+                                         {
+                                             "Action": "sts:AssumeRole",
+                                             "Principal": {
+                                                 "Service": "lambda.amazonaws.com"
+                                             },
+                                             "Effect": "Allow",
+                                             "Sid": ""
+                                         }
+                                     ]
+                                 }""")
+        self.policy = aws.iam.RolePolicy(f"{name}-policy",
+                                         role=self.role.id,
+                                         policy="""{
+                                             "Version": "2012-10-17",
+                                             "Statement": [
+                                                 {
+                                                     "Effect": "Allow",
+                                                     "Action": [
+                                                         "logs:CreateLogGroup",
+                                                         "logs:CreateLogStream",
+                                                         "logs:PutLogEvents"
+                                                     ],
+                                                     "Resource": "arn:aws:logs:*:*:*"
+                                                 },
+                                                 {
+                                                     "Effect": "Allow",
+                                                     "Action": [
+                                                         "ecr:GetDownloadUrlForLayer",
+                                                         "ecr:BatchGetImage",
+                                                         "ecr:BatchCheckLayerAvailability"
+                                                     ],
+                                                     "Resource": "*"
+                                                 },
+                                                 {
+                                                     "Effect": "Allow",
+                                                     "Action": [
+                                                         "ec2:CreateNetworkInterface",
+                                                         "ec2:DescribeNetworkInterfaces",
+                                                         "ec2:DeleteNetworkInterface"
+                                                     ],
+                                                     "Resource": "*"
+                                                 }
+                                             ]
+                                         }""")
+        pulumi.export("lambda_role_arn", self.role.arn)
+
+
+class SecurityGroup:
+    def __init__(self, name: str, vpc_id: pulumi.Output[str]):
+        self.security_group = aws.ec2.SecurityGroup(name,
+                                                    vpc_id=vpc_id,
+                                                    egress=[{
+                                                        "protocol": "-1",
+                                                        "from_port": 0,
+                                                        "to_port": 0,
+                                                        "cidr_blocks": ["0.0.0.0/0"],
+                                                    }],
+                                                    tags={"Name": name})
+        pulumi.export("lambda_security_group_id", self.security_group.id)
+
+
+# Create the resources using the classes
+vpc = VPC("my-vpc", "10.0.0.0/16")
+private_subnet = Subnet("private-subnet", vpc.vpc.id, "10.0.1.0/24", "us-east-1a")
+ecr_repo = ECRRepository("my-lambda-function")
+lambda_role = LambdaRole("lambda-role")
+lambda_security_group = SecurityGroup("lambda-security-group", vpc.vpc.id)
