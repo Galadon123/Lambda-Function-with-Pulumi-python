8497769f6349810e9f1395bd6b0706e8e1ab8416
Fazlul Karim <fazlulkarim362@gmail.com>
2024-06-29 12:27:26 +0600

    fasf

--- deploy-in-lambda/index.js
+++ deploy-in-lambda/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     console.log("Event: ", event);
     const response = {
         statusCode: 200,
-        body: JSON.stringify('hello world \n Yasin bila   ! '),
+        body: JSON.stringify('hello world \n Yasin bila   fasf ! '),
     };
     return response;
 };
