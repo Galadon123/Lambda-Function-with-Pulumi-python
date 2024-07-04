d97646b12a1ec1712c1a15e1f1488a2f9242404f
Fazlul Karim <fazlulkarim362@gmail.com>
2024-07-05 01:35:49 +0600

    update-15

--- deploy-in-lambda/index.js
+++ deploy-in-lambda/index.js
@@ -1,6 +1,8 @@
+const awsServerlessExpress = require('aws-serverless-express');
 const { initializeAndFetch } = require('./initialization/initialization');
 const { app, server } = require('./routes/routes');
 
+// Initialize and fetch before handling requests
 initializeAndFetch().catch((error) => {
   console.error("Initialization failed:", error);
   process.exit(1); // Exit Lambda function on initialization failure
