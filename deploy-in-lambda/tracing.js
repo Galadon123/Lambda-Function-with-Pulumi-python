1d99b68b3125e2d423e80c88131c21c3e11da85f
Fazlul Karim <fazlulkarim362@gmail.com>
2024-07-07 01:36:19 +0600

    upd

--- deploy-in-lambda/tracing.js
+++ deploy-in-lambda/tracing.js
@@ -31,7 +31,7 @@ const initializeTracer = async () => {
     // Configure OpenTelemetry exporter with dynamic IP
     const exporter = new CollectorTraceExporter({
       serviceName: 'my-lambda-function',
-      url: `http://10.0.1.214:4318`, // Replace with your OTel collector URL
+      url: `http://34.201.69.207:4317`, // Replace with your OTel collector URL
     });
 
     // Add span processor and register provider
