2a01bd2ec46e83fd3422e6ccf7d7d8e436ad5664
Fazlul Karim <fazlulkarim362@gmail.com>
2024-07-07 01:57:39 +0600

    fafa

--- deploy-in-lambda/index.js
+++ deploy-in-lambda/index.js
@@ -3,63 +3,73 @@ const initializeTracer = require('./tracing');
 exports.handler = async (event) => {
   let response;
   let tracer;
+  let span;
 
   try {
     tracer = await initializeTracer();
-    const span = tracer.startSpan('lambda-handler');
+    span = tracer.startSpan('lambda-handler');
 
-    // Use a context to propagate the span
-    await tracer.withSpan(span, async () => {
-      switch (event.httpMethod) {
-        case 'GET':
-          if (event.path === '/default/my-lambda-function') {
-            const routeSpan = tracer.startSpan('handle-default-route');
-            response = {
-              statusCode: 200,
-              body: JSON.stringify('Hello from Lambda!'),
-            };
-            routeSpan.end();
-          } else if (event.path === '/default/my-lambda-function/test1') {
-            const routeSpan = tracer.startSpan('handle-test1-route');
-            response = {
-              statusCode: 200,
-              body: JSON.stringify('This is test1 route!'),
-            };
-            routeSpan.end();
-          } else if (event.path === '/default/my-lambda-function/test2') {
-            const routeSpan = tracer.startSpan('handle-test2-route');
-            response = {
-              statusCode: 200,
-              body: JSON.stringify('This is test2 route!'),
-            };
-            routeSpan.end();
-          } else {
-            const routeSpan = tracer.startSpan('handle-not-found');
-            response = {
-              statusCode: 404,
-              body: JSON.stringify('Not Found'),
-            };
-            routeSpan.end();
-          }
-          break;
-        default:
-          const methodNotAllowedSpan = tracer.startSpan('method-not-allowed');
-          response = {
-            statusCode: 405,
-            body: JSON.stringify('Method Not Allowed'),
-          };
-          methodNotAllowedSpan.end();
+    // Set the current span as active
+    tracer.startActiveSpan('lambda-handler', async (parentSpan) => {
+      try {
+        switch (event.httpMethod) {
+          case 'GET':
+            if (event.path === '/default/my-lambda-function') {
+              tracer.startActiveSpan('handle-default-route', (routeSpan) => {
+                response = {
+                  statusCode: 200,
+                  body: JSON.stringify('Hello from Lambda!'),
+                };
+                routeSpan.end();
+              });
+            } else if (event.path === '/default/my-lambda-function/test1') {
+              tracer.startActiveSpan('handle-test1-route', (routeSpan) => {
+                response = {
+                  statusCode: 200,
+                  body: JSON.stringify('This is test1 route!'),
+                };
+                routeSpan.end();
+              });
+            } else if (event.path === '/default/my-lambda-function/test2') {
+              tracer.startActiveSpan('handle-test2-route', (routeSpan) => {
+                response = {
+                  statusCode: 200,
+                  body: JSON.stringify('This is test2 route!'),
+                };
+                routeSpan.end();
+              });
+            } else {
+              tracer.startActiveSpan('handle-not-found', (routeSpan) => {
+                response = {
+                  statusCode: 404,
+                  body: JSON.stringify('Not Found'),
+                };
+                routeSpan.end();
+              });
+            }
+            break;
+          default:
+            tracer.startActiveSpan('method-not-allowed', (methodSpan) => {
+              response = {
+                statusCode: 405,
+                body: JSON.stringify('Method Not Allowed'),
+              };
+              methodSpan.end();
+            });
+        }
+      } finally {
+        parentSpan.end();
       }
     });
 
-    span.end();
     return response;
   } catch (error) {
     console.error('Error:', error);
     if (tracer) {
-      const errorSpan = tracer.startSpan('error-handler');
-      errorSpan.recordException(error);
-      errorSpan.end();
+      tracer.startActiveSpan('error-handler', (errorSpan) => {
+        errorSpan.recordException(error);
+        errorSpan.end();
+      });
     }
     return {
       statusCode: 500,
@@ -67,4 +77,3 @@ exports.handler = async (event) => {
     };
   }
 };
-
