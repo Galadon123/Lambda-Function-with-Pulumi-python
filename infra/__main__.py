7c5e794d1f02171a0eaf1c032ad7b6516323e57c
Fazlul Karim <fazlulkarim362@gmail.com>
2024-07-07 21:08:46 +0600

    faf

--- infra/__main__.py
+++ infra/__main__.py
@@ -13,7 +13,7 @@ igw = aws.ec2.InternetGateway("my-vpc-igw",
                               tags={"Name": "my-vpc-igw"})
 
 
-# Create Route Table for Public Subnets
+# Create Route Table for Public Subnet
 public_route_table = aws.ec2.RouteTable("my-vpc-public-rt",
                                         vpc_id=vpc.id,
                                         routes=[{
