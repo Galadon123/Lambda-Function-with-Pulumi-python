193f9cacd8e99926fd09033b6168b95549211699
Fazlul Karim <fazlulkarim362@gmail.com>
2024-07-07 22:00:41 +0600

    api

--- API-Gateway/__main__.py
+++ API-Gateway/__main__.py
@@ -1,10 +1,43 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+# Fetch Lambda function ARN from S3
+lambda_function_arn = pulumi.Output.from_input(
+    aws.s3.get_object(bucket="lambda-function-bucket-poridhi", key="lambda-function-arn.json")
+).apply(lambda obj: obj["Body"].read().decode("utf-8"))
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# Define your API Gateway
+api = aws.apigatewayv2.Api("my-api", protocol_type="HTTP", target="AWS_LAMBDA", tags={"Name": "my-api"})
+
+# Define Lambda integration for API Gateway using fetched ARN
+integration1 = aws.apigatewayv2.Integration("lambda-integration",
+    api_id=api.id,
+    integration_type="AWS_PROXY",
+    integration_method="POST",
+    integration_uri=lambda_function_arn,
+    integration_timeout_milliseconds=3000,
+    payload_format_version="2.0",
+    tags={"Name": "lambda-integration"},
+)
+
+# Define routes for API Gateway
+route1 = aws.apigatewayv2.Route("my-route",
+    api_id=api.id,
+    route_key="ANY /my-lambda-function",
+    target=integration1.id,
+)
+
+route2 = aws.apigatewayv2.Route("test2-route",
+    api_id=api.id,
+    route_key="ANY /test2",
+    target=integration1.id,
+)
+
+route3 = aws.apigatewayv2.Route("test1-route",
+    api_id=api.id,
+    route_key="ANY /test1",
+    target=integration1.id,
+)
+
+# Export API Gateway endpoint URL
+pulumi.export("api_url", api.api_endpoint)
