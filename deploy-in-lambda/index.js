58dd41437a97e49b12af1bbd6df439d9071db132
Fazlul Karim <fazlulkarim362@gmail.com>
2024-07-07 23:09:34 +0600

    fsfas

--- deploy-in-lambda/index.js
+++ deploy-in-lambda/index.js
@@ -15,7 +15,7 @@ exports.handler = async (event) => {
     };
   }
 
-  const tracer = trace.getTracer('default'); // Get the tracer from the OpenTelemetry s
+  const tracer = trace.getTracer('default'); // Get the tracer from the OpenTelemetry ss
   const span = tracer.startSpan('lambda-handler');
   try {
     let response;
