f6aff49175f0b3fdcf32317254e480d2a1f126fb
Fazlul Karim <fazlulkarim362@gmail.com>
2024-07-08 13:07:23 +0600

    bucket-name-updated-2

--- infra/__main__.py
+++ infra/__main__.py
@@ -13,7 +13,7 @@ igw = aws.ec2.InternetGateway("my-vpc-igw",
                               tags={"Name": "my-vpc-igw"})
 
 
-# Create Route Table for Publicss
+# Create Route Table for Public
 public_route_table = aws.ec2.RouteTable("my-vpc-public-rt",
                                         vpc_id=vpc.id,
                                         routes=[{
